161f27f684f9b863640c33ecb81469a3eab94a5b
desema <desema.s7@gmail.com>
2021-06-06 19:01:48 +0300

    task3

--- task3.tf
+++ task3.tf
@@ -3,7 +3,7 @@ module "ec2_cluster" {
   version                = "~> 2.0"
 
   name                   = "test-cluster"
-  instance_count         = 3
+  instance_count         = 2
 
   ami                    = "ami-0cf6f5c8a62fa5da6"
   instance_type          = "t2.micro"
